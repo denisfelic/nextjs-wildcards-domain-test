b5f286c2710510834a3032137a39b0c30cb216c8
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-01 21:09:56 -0300

    add catalog page

---
+++ pages/catalog.jsx
@@ -0,0 +1,27 @@
+export default function Catalog(props) {
+  return (
+    <div>Catalogo - {props.fullUrl}</div>
+  )
+}
+
+Catalog.getInitialProps = async (ctx) => {
+  const req = ctx.req;
+  let fullUrl
+  if (req) {
+    // Server side rendering
+    fullUrl = req.headers.host?.split(':')[0];
+  } else {
+    // Client side rendering
+    fullUrl = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '')
+  }
+
+  if (fullUrl.split('.')[0].length < 3) {
+    ctx.res.writeHead(302, { // or 301
+      Location: "http://denis-dev:3000/",
+
+    });
+    res.end();
+  }
+
+  return { fullUrl: fullUrl }
+}
