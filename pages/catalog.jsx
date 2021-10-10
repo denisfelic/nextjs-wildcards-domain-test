e33c066cf31e1fc10f63bce84ffa4611ac1d45dc
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-10 01:38:20 -0300

    get data from .env file

--- pages/catalog.jsx
+++ pages/catalog.jsx
@@ -6,22 +6,25 @@ export default function Catalog(props) {
 
 Catalog.getInitialProps = async (ctx) => {
   const req = ctx.req;
+  const res = ctx.res;
   let fullUrl
   if (req) {
     // Server side rendering
     fullUrl = req.headers.host?.split(':')[0];
+    // TODO: Refactor to make get this from database
+    if (fullUrl.split('.')[0].length < 3 || fullUrl == 'localhost') {
+      res.writeHead(302, { // or 301
+        Location: process.env.DEV == 'true' ? process.env.DOMAIN_DEV : process.env.DOMAIN_PROD,
+
+      });
+      res.end();
+    }
   } else {
     // Client side rendering
     fullUrl = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '')
   }
 
-  if (fullUrl.split('.')[0].length < 3) {
-    ctx.res.writeHead(302, { // or 301
-      Location: "https://www.denis-dev/",
 
-    });
-    res.end();
-  }
 
   return { fullUrl: fullUrl }
 }
