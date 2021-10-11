32f2d27aada872ae37f5a23a92e77396578fafd7
Denis Feliciano da Silva <denisf.salles@gmail.com>
2021-10-11 00:29:36 -0300

    refactor to use HandleSubdomain function

--- pages/catalog.jsx
+++ pages/catalog.jsx
@@ -1,32 +1,11 @@
+import HandleSubdomain from "../Helpers/HandleSubdomain";
+
 export default function Catalog(props) {
   return (
     <div>Catalogo - {props.fullUrl}</div>
   )
 }
 
-Catalog.getInitialProps = async (ctx) => {
-  const req = ctx.req;
-  const res = ctx.res;
-  let fullUrl
-  if (req) {
-    // Server side rendering
-    fullUrl = req.headers.host?.split(':')[0];
-    // TODO: Refactor to make get this from database
-    console.log(process.env.DEV)
-    console.log(process.env.DOMAIN_PROD)
-    if (fullUrl.split('.')[0].length < 3 || fullUrl == 'localhost') {
-      res.writeHead(302, { // or 301
-        Location: (process.env.DEV == 'true' || process.env.DEV == true) ? process.env.DOMAIN_DEV : process.env.DOMAIN_PROD,
-
-      });
-      res.end();
-    }
-  } else {
-    // Client side rendering
-    fullUrl = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '')
-  }
-
-
-
-  return { fullUrl: fullUrl }
+export async function getServerSideProps(ctx) {
+  return HandleSubdomain(ctx);
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,5 @@
+import HandleSubdomain from "../Helpers/HandleSubdomain";
+import Head from 'next/head';
 
 
 async function getStaticPaths() {
@@ -7,37 +9,14 @@ async function getStaticPaths() {
   }
 }
 
-const Home = ({ fullUrl }) => {
-
-  const data = [
-    {
-      name: 'Sociedade Esportiva Palmeiras',
-      hostname: 'palmeiras',
-      products: [
-        { name: 'Camiseta Masculina', price: 129.99, sizes: ['P', 'M', 'G'], picture: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQXpfZVRMnfAczBuyc-cp-FoeGSZ9VetOqmpVCwPir8NU5P2s_1rxaFZPXq02azleifO7QPoa8mfXg&usqp=CAc' },
-        { name: 'Camiseta Feminina', price: 129.99, sizes: ['P', 'M', 'G'], picture: 'https://imgcentauro-a.akamaihd.net/900x900/96008401/camisa-do-palmeiras-ii-21-puma-feminina-img.jpg' },
-      ]
-    },
-    {
-      name: 'Grêmio',
-      hostname: 'gremio',
-      products: [
-        { name: 'Camiseta Masculina', price: 100, sizes: ['P', 'M', 'G'], picture: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQAYMBnpxREu6YR2Wy-fq0qbAfJ4DK9AtWXyhhKGEB_awJulZSgjxGdZ43j7FK2J16l25nnpECTJps&usqp=CAc' },
-        { name: 'Camiseta Feminina', price: 100, sizes: ['P', 'M', 'G'], picture: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ6XMeinI4iEQ_WcpCGQ4CSEzaZjKa1_2jt__4wAh9AsVTuXXNS1-K6GrEv45hET_AGHWWkAAtdUQ&usqp=CAc' },
-      ]
-    }
-  ];
-
-
-  let subdomain = fullUrl.split('.')[0];
-
-  let club = data.find(e => e.hostname === subdomain)
-  console.log(club)
-
-
-
+const Home = ({ club }) => {
   return (
     <div >
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      </Head>
       {club ? <DomainPage club={club} /> : <Institutional />}
     </div>
   )
@@ -71,29 +50,8 @@ function DomainPage({ club }) {
   )
 }
 
-Home.getInitialProps = async (ctx) => {
-  const req = ctx.req;
-  const res = ctx.res;
-  let fullUrl
-  if (req) {
-    // Server side rendering
-    fullUrl = req.headers.host?.split(':')[0];
-    // TODO: Refactor to make get this from database
-    if (fullUrl.split('.')[0].length < 3) {
-      res.writeHead(302, { // or 301
-        Location: "https://www.denis-dev.com",
-
-      });
-      res.end();
-    }
-  } else {
-    // Client side rendering
-    fullUrl = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '')
-  }
-
-
-
-  return { fullUrl: fullUrl }
+export async function getServerSideProps(ctx) {
+  return HandleSubdomain(ctx);
 }
 
 export default Home
